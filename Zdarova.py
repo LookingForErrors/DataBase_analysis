66953bd75c8be421a0b51a3b215905d379b08a00
lferrors <danila_petrov_02@inbox.ru>
2024-08-08 21:04:31 +0000

    testCommit

---
+++ Zdarova.py
@@ -0,0 +1,10 @@
+import sqlalchemy as db
+import os
+
+engine = db.create_engine(os.environ["DB_URL"])
+
+with engine.connect() as connection:
+    result = connection.execute(db.text("SELECT * FROM my_table"))
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
